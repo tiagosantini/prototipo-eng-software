aa9d732f38d012867e196da148bc71dbdcc84566
Tiago <tiagosantiniathayde@outlook.com>
2022-03-14 15:08:27 -0300

    Movendo projeto do servidor

---
+++ src/server/PrototipoEngSoftware.Dominio/ContaModule/Conta.cs
@@ -0,0 +1,30 @@
+﻿using PrototipoEngSoftware.Dominio.Shared;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrototipoEngSoftware.Dominio.ContaModule
+{
+    [Table("TBConta")]
+    public class Conta : Entidade
+    {
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string CPF { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        public IEnumerable<Movimentacao> Movimentacoes { get; private set; }
+
+        public Conta()
+        {
+        }
+    }
+}
